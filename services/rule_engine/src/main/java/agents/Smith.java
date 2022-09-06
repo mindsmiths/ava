65e38e46425232dd5b55a61a51eb8e423a59e1cb
ivan.dukic <ivandukic97@gmail.com>
2022-09-06 16:05:50 +0200

    Added sendEmail method to Smith.

--- services/rule_engine/src/main/java/agents/Smith.java
+++ services/rule_engine/src/main/java/agents/Smith.java
@@ -1,5 +1,9 @@
 package agents;
 
+import java.util.List;
+
+import com.mindsmiths.emailAdapter.EmailAdapterAPI;
+import com.mindsmiths.emailAdapter.SendEmailPayload;
 import com.mindsmiths.ruleEngine.model.Agent;
 import lombok.Data;
 import lombok.ToString;
@@ -11,5 +15,13 @@ public class Smith extends Agent {
 
     public Smith() {
         this.Id = "SMITH";
+    }
+
+    public void sendEmail(String recipient, String subject, String text) {
+        SendEmailPayload e = new SendEmailPayload();
+        e.setRecipients(List.of(recipient));
+        e.setSubject(subject);
+        e.setPlainText(text);
+        EmailAdapterAPI.newEmail(e);
     }
 }
