832a213bda9bb20d3e296d163bdb6bf58b22f8dd
ivan.dukic <ivandukic97@gmail.com>
2022-09-06 08:49:20 +0200

    First commit.

---
+++ services/rule_engine/src/main/java/Runner.java
@@ -0,0 +1,18 @@
+import com.mindsmiths.ruleEngine.runner.RuleEngineService;
+import com.mindsmiths.ruleEngine.util.Agents;
+import com.mindsmiths.ruleEngine.util.Signals;
+
+
+public class Runner extends RuleEngineService {
+    @Override
+    public void initialize() {
+        configureSignals(
+            // TODO: listen to signals here
+        );
+    }
+
+    public static void main(String[] args) {
+        Runner runner = new Runner();
+        runner.start();
+    }
+}
