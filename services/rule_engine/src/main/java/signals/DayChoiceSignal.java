9dbaae42ddeffb2454f3c645d1006145410ef8ce
filbac1 <filipbacic08@gmail.com>
2022-09-06 17:39:19 +0200

    Fixed bugs in previous version

--- services/rule_engine/src/main/java/signals/DayChoiceSignal.java
+++ services/rule_engine/src/main/java/signals/DayChoiceSignal.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import com.mindsmiths.sdk.core.api.Signal;
 
 public class DayChoiceSignal extends Signal {
+    ArrayList<Integer> freeDays;
 
     public DayChoiceSignal(ArrayList<Integer> freeDays) {
     }
