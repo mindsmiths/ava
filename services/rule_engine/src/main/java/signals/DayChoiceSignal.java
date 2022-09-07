a8c5597e2883bf87486807de2f2fadd6fce19f7a
Ana Maric <anamari040@gmail.com>
2022-09-07 11:34:05 +0200

    Merge conflict

--- services/rule_engine/src/main/java/signals/DayChoiceSignal.java
+++ services/rule_engine/src/main/java/signals/DayChoiceSignal.java
@@ -2,15 +2,12 @@ package signals;
 
 import java.util.ArrayList;
 
+import lombok.AllArgsConstructor;
+
 import com.mindsmiths.sdk.core.api.Signal;
 
-import lombok.Data;
 
-@Data
+@AllArgsConstructor
 public class DayChoiceSignal extends Signal {
-    ArrayList<Integer> freeDays;
-
-    public DayChoiceSignal(ArrayList<Integer> freeDays) {
-    }
-    
+    ArrayList<Integer> freeDays;    
 }
