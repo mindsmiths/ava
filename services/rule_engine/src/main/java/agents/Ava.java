3332111be63b994a9d01676afef50d7ab3de929c
ivan.dukic <ivandukic97@gmail.com>
2022-09-07 16:54:43 +0200

    Messages added through Mitems.

--- services/rule_engine/src/main/java/agents/Ava.java
+++ services/rule_engine/src/main/java/agents/Ava.java
@@ -55,44 +55,54 @@ public class Ava extends Agent {
      }
 
     public void showFamiliarityQuizScreens() {
+        String question1 = Mitems.getText("onboarding.familiarity-quiz.question1");
+        String question2 = Mitems.getText("onboarding.familiarity-quiz.question2");
+        String finishfamiliarityquiz = Mitems.getText("onboarding.familiarity-quiz.finishfamiliarityquiz");
+        Option[] nextbutton = Mitems.getOptions("onboarding.familiarity-quiz.nextbutton");
         Map<String, BaseTemplate> screens = Map.of(
                 "question1", new TemplateGenerator("question1")
-                        .addComponent("question", new TitleComponent("Who of the following do you know best?"))
+                        .addComponent("question", new TitleComponent(question1))
                         .addComponent("actionGroup", new ActionGroupComponent(List.of(
                             new PrimarySubmitButtonComponent("Option1","Option 1", "question2"),
                             new PrimarySubmitButtonComponent("Option2","Option 2", "question2"),
                             new PrimarySubmitButtonComponent("Option3","Option 3", "question2")))),
                 "question2", new TemplateGenerator("question2")
-                        .addComponent("question", new TitleComponent("Who of the following do you know best?"))
+                        .addComponent("question", new TitleComponent(question2))
                         .addComponent("actionGroup", new ActionGroupComponent(List.of(
                             new PrimarySubmitButtonComponent("Option 1", "Option 1", "finishFamiliarityQuiz"),
                             new PrimarySubmitButtonComponent("Option 2", "Option 2", "finishFamiliarityQuiz"),
                             new PrimarySubmitButtonComponent("Option 3", "Option 3", "finishFamiliarityQuiz")))),
-                "finishFamiliarityQuiz", new TemplateGenerator("finishFamiliarityQuiz")
+                "finishFamiliarityQuiz", new TemplateGenerator(finishfamiliarityquiz)
                             .addComponent("title", new TitleComponent("Thanks! I’ll check in with you from time to time to ask about other people too, but for now I have enough to get us started. Ready for fun part?"))
-                            .addComponent("button", new PrimarySubmitButtonComponent("button", "Sure thing", "finished"))  
+                            .addComponent("button", new PrimarySubmitButtonComponent("button", nextbutton[0].getText(), "finished"))  
         );
         showScreens("question1", screens);
     }
 
     public void showPersonalQuizScreens() {
+        String finishpersonalquiz = Mitems.getText("onboarding.personal-quiz.finishpersonalquiz");
+        String goodbye_screen = Mitems.getText("onboarding.personal-quiz.goodbye-screen");
+        String intro_screen = Mitems.getText("onboarding.personal-quiz.intro-screen");
+        String question1 = Mitems.getText("onboarding.personal-quiz.question1");
+        String question2 = Mitems.getText("onboarding.personal-quiz.question2");
+
         Map<String, BaseTemplate> screens = Map.of(
             "introScreen", new TemplateGenerator("introScreen")
-            .addComponent("title", new TitleComponent("Okay, first part done! Now, the fun part!I prepared 5 questions for you and your colleagues. Ready to start?"))
+            .addComponent("title", new TitleComponent(intro_screen))
             .addComponent("button", new PrimarySubmitButtonComponent("button", "Let's do it", "question1")),
                 "question1", new TemplateGenerator("question1")
-                        .addComponent("question", new TitleComponent("What is the name of the last song you listened to?"))
+                        .addComponent("question", new TitleComponent(question1))
                         .addComponent("answer", new InputComponent("answer", "Type your answer here", true))
                         .addComponent("submit", new PrimarySubmitButtonComponent("submit", "Submit", "question2")),
                 "question2", new TemplateGenerator("question2")
-                        .addComponent("question", new TitleComponent("What is the name of the last TV show you watched?"))
+                        .addComponent("question", new TitleComponent(question2))
                         .addComponent("answer", new InputComponent("answer", "Type your answer here", true))
-                        .addComponent("actionGroup", new PrimarySubmitButtonComponent("Option1","Option 1", "finishPersonalQuiz")),                       
+                        .addComponent("button", new PrimarySubmitButtonComponent("Option1","Option 1", "finishPersonalQuiz")),                       
                 "finishPersonalQuiz", new TemplateGenerator("finishPersonalQuiz")
-                            .addComponent("title", new TitleComponent("You are officially onboarded! Soon I’ll send you an email with connectivity data. Stay tuned!"))
+                            .addComponent("title", new TitleComponent(finishpersonalquiz))
                             .addComponent("button", new PrimarySubmitButtonComponent("button", "Great, can't wait!", "goodbye")),
                 "goodbye", new TemplateGenerator("goodbye")
-                            .addComponent("title", new TitleComponent("You are the best :*"))
+                            .addComponent("title", new TitleComponent(goodbye_screen))
         );
         showScreens("question1", screens);
     }
