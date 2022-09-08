c8145e0679f816f457eb8da7e5464d04e2d7df41
Ana Maric <anamari040@gmail.com>
2022-09-08 17:48:29 +0200

    Improved version

--- services/rule_engine/src/main/java/agents/Ava.java
+++ services/rule_engine/src/main/java/agents/Ava.java
@@ -1,10 +1,13 @@
 package agents;
 
+import com.mindsmiths.mitems.Mitems;
+
 import java.util.*;
 
 import com.mindsmiths.armory.ArmoryAPI;
 import com.mindsmiths.armory.templates.*;
 import com.mindsmiths.armory.components.*;
+import com.mindsmiths.armory.components.CloudSelectComponent.Option;
 import com.mindsmiths.ruleEngine.model.Agent;
 
 import lombok.*;
@@ -21,7 +24,7 @@ public class Ava extends Agent {
         super(connectionName, connectionId);
     }
 
-    public void sendData(ArrayList<String> notFreeDays) {
+    public void sendDayChoice(ArrayList<String> notFreeDays) {
         send("CultureMaster", new DayChoiceSignal(notFreeDays));
     }
 
@@ -33,33 +36,42 @@ public class Ava extends Agent {
         ArmoryAPI.showScreens(getConnection("armory"), firstScreenId, screens);
     }
 
-    public void bestTimeScreen() {
+    public void chooseAvailableDaysScreen() {
+        String title = Mitems.getText("weekly-core.title-asking-for-available-days.title");
+        String description = Mitems.getText("weekly-core.description-asking-for-available-days.text");
+        String mon = Mitems.getText("weekly-core.days.monday");
+        String tue = Mitems.getText("weekly-core.days.tuesday");
+        String wed = Mitems.getText("weekly-core.days.wednsday");
+        String thu = Mitems.getText("weekly-core.days.thursday");
+        String fri = Mitems.getText("weekly-core.days.friday");
+
         List<CloudSelectComponent.Option> options = List.of(
-            new CloudSelectComponent.Option("Mon", "1", true),
-            new CloudSelectComponent.Option("Tue", "2", true),
-            new CloudSelectComponent.Option("Wed", "3", true),
-            new CloudSelectComponent.Option("Thu", "4", true),
-            new CloudSelectComponent.Option("Fri", "5", true)
+            new CloudSelectComponent.Option(mon, "1", true),
+            new CloudSelectComponent.Option(tue, "2", true),
+            new CloudSelectComponent.Option(wed, "3", true),
+            new CloudSelectComponent.Option(thu, "4", true),
+            new CloudSelectComponent.Option(fri, "5", true)
         );
+
         BaseTemplate daysScreen = new TemplateGenerator()
-            .addComponent("title", new TitleComponent("When works best?"))
-            .addComponent("text", new DescriptionComponent("Tick off the boxes of days when you are not free."))
+            .addComponent("title", new TitleComponent(title))
+            .addComponent("text", new DescriptionComponent(description))
             .addComponent("cloudSelect", new CloudSelectComponent("availableDays", options))
             .addComponent("submit", new PrimarySubmitButtonComponent("submit", "confirmDays"));
         showScreen(daysScreen);
     }
 
-    public void confirmationScreen1() {
-        BaseTemplate thanksScreen1 = new TemplateGenerator()
-            .addComponent("title", new TitleComponent("Okay great, I'll send you an email when I catch up with others :)"))
-            .addComponent("submit", new PrimarySubmitButtonComponent("Okay, thanks :)", "confirmThanks"));
-        showScreen(thanksScreen1);
-    }
-
-    public void confirmationScreen2() {
-        BaseTemplate thanksScreen2 = new TemplateGenerator()
-            .addComponent("title", new TitleComponent("You're welcome. Stay tuned for the lunch :)"));
-        showScreen(thanksScreen2);
+    public void confirmingDaysScreen() {
+        String title1 = Mitems.getText("weekly-core.confirmation-of-choosen-available-days.title");
+        String title2 = Mitems.getText("weekly-core.stay-tuned-second-confirmation-of-available-days.title");
+        Map<String, BaseTemplate> screens = Map.of(
+            "confirmDaysScreen", new TemplateGenerator("confirmScreen")
+                .addComponent("title", new TitleComponent(title1))
+                .addComponent("button", new PrimarySubmitButtonComponent("submit", "confirmDaysAndThanksScreen")),
+            "confirmDaysAndThanksScreen", new TemplateGenerator("confirmAndThanksScreen")
+                .addComponent("title", new TitleComponent(title2))
+        );
+    showScreens("confirmDaysScreen", screens);
     }
 
 }
