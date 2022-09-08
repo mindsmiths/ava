372fe5c047d4be562e440f7a9adb4933d072fa16
Ana Maric <anamari040@gmail.com>
2022-09-08 14:44:42 +0200

    Implementation of choosing days

--- services/rule_engine/src/main/java/agents/Ava.java
+++ services/rule_engine/src/main/java/agents/Ava.java
@@ -1,12 +1,13 @@
 package agents;
 
-import java.util.ArrayList;
+import java.util.*;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
+import com.mindsmiths.armory.ArmoryAPI;
+import com.mindsmiths.armory.templates.*;
+import com.mindsmiths.armory.components.*;
 import com.mindsmiths.ruleEngine.model.Agent;
+
+import lombok.*;
 
 import signals.DayChoiceSignal;
 
@@ -20,8 +21,48 @@ public class Ava extends Agent {
         super(connectionName, connectionId);
     }
 
-    public void sendData(ArrayList<Integer> freeDays) {
-        send("CultureMaster", new DayChoiceSignal(freeDays));
+    public void sendData(ArrayList<String> notFreeDays) {
+        send("CultureMaster", new DayChoiceSignal(notFreeDays));
     }
- 
+
+    public void showScreen(BaseTemplate screen) {
+        ArmoryAPI.showScreen(getConnection("armory"), screen);
+    }
+
+    public void showScreens(String firstScreenId, Map<String, BaseTemplate> screens) {
+        ArmoryAPI.showScreens(getConnection("armory"), firstScreenId, screens);
+    }
+
+    public void bestTimeScreen() {
+        List<CloudSelectComponent.Option> options = List.of(
+            new CloudSelectComponent.Option("Mon", "1", true),
+            new CloudSelectComponent.Option("Tue", "2", true),
+            new CloudSelectComponent.Option("Wed", "3", true),
+            new CloudSelectComponent.Option("Thu", "4", true),
+            new CloudSelectComponent.Option("Fri", "5", true)
+        );
+        BaseTemplate daysScreen = new TemplateGenerator()
+            .addComponent("title", new TitleComponent("When works best?"))
+            .addComponent("text", new DescriptionComponent("Tick off the boxes of days when you are not free."))
+            .addComponent("cloudSelect", new CloudSelectComponent("availableDays", options))
+            .addComponent("submit", new PrimarySubmitButtonComponent("submit", "confirmDays"));
+        showScreen(daysScreen);
+    }
+
+    public void confirmationScreen1() {
+        BaseTemplate thanksScreen1 = new TemplateGenerator()
+            .addComponent("title", new TitleComponent("Okay great, I'll send you an email when I catch up with others :)"))
+            .addComponent("submit", new PrimarySubmitButtonComponent("Okay, thanks :)", "confirmThanks"));
+        showScreen(thanksScreen1);
+    }
+
+    public void confirmationScreen2() {
+        BaseTemplate thanksScreen2 = new TemplateGenerator()
+            .addComponent("title", new TitleComponent("You're welcome. Stay tuned for the lunch :)"));
+        showScreen(thanksScreen2);
+    }
+
+    //kako spriječiti osobu da ne ispuni ponovno nakon sumbita refreshanjem ili ponovnim otvaranjem linka??
+
+    //kako postaviti da se svaki ovaj iz određenog maila otvori
 }
--- services/rule_engine/src/main/java/signals/DayChoiceSignal.java
+++ services/rule_engine/src/main/java/signals/DayChoiceSignal.java
@@ -1,13 +1,17 @@
 package signals;
 
-import java.util.ArrayList;
-
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
 
 import com.mindsmiths.sdk.core.api.Signal;
 
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class DayChoiceSignal extends Signal {
-    ArrayList<Integer> freeDays;    
+    ArrayList<String> notFreeDays;    
 }
