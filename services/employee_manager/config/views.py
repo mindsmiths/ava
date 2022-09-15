e7767d04389937d104d23784e9cf95992ea94784
mmatichr <mislavmatichr@gmail.com>
2022-09-15 10:41:57 +0200

    adds familiarity quiz trigger

--- services/employee_manager/config/views.py
+++ services/employee_manager/config/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 from rule_engine.api import RuleEngineAPI
 from forge.core.api.events import Signal
+from django.contrib import messages
 
 
 def home(request):
@@ -9,6 +10,9 @@ def home(request):
 
 def trigger(request, trigger_type):
     to = "CULTURE_MASTER"
-    signal = Signal(type="ManualTriggerEvent", from_="dashboard", to=to, triggerType=trigger_type)
+    signal = Signal(type="ManualTriggerEvent", from_="dashboard", to=to,
+                    triggerType=trigger_type)
     RuleEngineAPI.send_signal(to, signal)
+    messages.success(request,
+                     f'Action "{trigger_type}" trigger sent succesfully!')
     return redirect('/admin/')
--- services/employee_manager/dashboard.py
+++ services/employee_manager/dashboard.py
@@ -9,7 +9,7 @@ class CustomIndexDashboard(Dashboard):
     def init_with_context(self, context):
         self.available_children.append(modules.LinkList)
         self.children.append(modules.LinkList(
-            _('Support'),
+            _('Actions'),
             children=[
                 {
                     'title': _('Trigger lunch cycle'),
@@ -18,6 +18,10 @@ class CustomIndexDashboard(Dashboard):
                 {
                     'title': _('Trigger lunch pairing'),
                     'url': '/trigger/lunch_pairing',
+                },
+                {
+                    'title': _('Trigger familiarity quiz'),
+                    'url': '/trigger/familiarity_quiz',
                 },
             ],
             column=0,
