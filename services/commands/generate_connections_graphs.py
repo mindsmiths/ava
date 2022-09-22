673037a7b947229395d069ef890c0b53675c6ea9
ivan.dukic <ivandukic97@gmail.com>
2022-09-22 11:43:04 +0000

    Added: generating connections graphs

---
+++ services/commands/generate_connections_graphs.py
@@ -0,0 +1,54 @@
+import forge
+from forge.utils.mongo import MongoClientKeeper
+from forge_cli.admin import cli
+
+import pandas as pd
+import networkx as nx
+from pyvis.network import Network
+import matplotlib.pyplot as plt
+
+@cli.command()
+def generate_connections_graphs():
+    forge.setup("rule_engine")
+    keeper = MongoClientKeeper()
+    culture_master = keeper.ruleEngineDB.summary.find_one({"agentId": "CULTURE_MASTER"})
+    """------------------------------------------------------------
+    culture_master["agents#CultureMaster"]["CULTURE_MASTER"][ "avaConnectionStrengths"] = {
+        '1': {
+            '2': 100, '3': 80
+            },
+        '2': {
+            '1': 80, '3': 20
+            }, 
+        '3': {
+            '1': 100, '2': 40
+            }
+        }
+    -------------------------------------------------------------"""
+    avaConnectionStrengths = pd.DataFrame.from_dict(
+        culture_master["agents#CultureMaster"]["CULTURE_MASTER"]["avaConnectionStrengths"]
+    )
+    employeeIDs = avaConnectionStrengths.columns
+    G2 = nx.Graph()
+    for i in range(len(employeeIDs)):
+        G2.add_node(employeeIDs[i])
+        for j in range(i+1, len(employeeIDs)):
+            score = 0
+            a = avaConnectionStrengths[employeeIDs[i]][employeeIDs[j]]
+            b = avaConnectionStrengths[employeeIDs[j]][employeeIDs[i]]
+        
+            if a>=50 and b>=50:
+                score = (a+b)/2
+                if score > 80:
+                    G2.add_edge(employeeIDs[i],employeeIDs[j],weight=score)
+                    print(employeeIDs[i],employeeIDs[j],score)
+    
+    for i in range(len(employeeIDs)):
+        color_map = len(employeeIDs) * ["royalblue"]
+        color_map[i] = "red"
+        plt.show()
+        nx.draw(G2, with_labels = False , node_color=color_map, width=2, edge_color = "royalblue")
+        plt.savefig("/app/services/commands/{}.png".format(i+1), format="PNG")
+        plt.close()
+
+generate_connections_graphs()
