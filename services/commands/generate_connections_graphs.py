16f5d90d3d6a408a4e7f141f1b6aa9a2773c677e
ivan.dukic <ivandukic97@gmail.com>
2022-09-22 11:48:11 +0000

    Deleted function call

--- services/commands/generate_connections_graphs.py
+++ services/commands/generate_connections_graphs.py
@@ -29,7 +29,6 @@ def generate_connections_graphs():
                 score = (a+b)/2
                 if score > 80:
                     G2.add_edge(employeeIDs[i],employeeIDs[j],weight=score)
-                    print(employeeIDs[i],employeeIDs[j],score)
     
     for i in range(len(employeeIDs)):
         color_map = len(employeeIDs) * ["royalblue"]
@@ -38,5 +37,3 @@ def generate_connections_graphs():
         nx.draw(G2, with_labels = False , node_color=color_map, width=2, edge_color = "royalblue")
         plt.savefig("/app/services/commands/{}.png".format(i+1), format="PNG")
         plt.close()
-
-generate_connections_graphs()
