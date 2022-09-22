5652b4d5671782106bdb1f6eab952ef2b77a637a
ivan.dukic <ivandukic97@gmail.com>
2022-09-22 14:15:41 +0000

    Merge request comments resolved

--- services/commands/generate_connections_graphs.py
+++ services/commands/generate_connections_graphs.py
@@ -13,33 +13,33 @@ def generate_connections_graphs():
     forge.setup("rule_engine")
     keeper = MongoClientKeeper()
     culture_master = keeper.ruleEngineDB.summary.find_one({"agentId": "CULTURE_MASTER"})
-    
-    avaConnectionStrengths = pd.DataFrame.from_dict(
+
+    ava_connection_strengths = pd.DataFrame.from_dict(
         culture_master["agents#CultureMaster"]["CULTURE_MASTER"]["avaConnectionStrengths"]
     )
-    employeeIDs = avaConnectionStrengths.columns
+    employee_ids = ava_connection_strengths.columns
     G2 = nx.Graph()
-    numOfEmployees = len(employeeIDs)
+    num_of_employees = len(employee_ids)
 
-    for i in range(numOfEmployees):
-        G2.add_node(employeeIDs[i])
-        for j in range(i+1, numOfEmployees):
+    for i in range(num_of_employees):
+        G2.add_node(employee_ids[i])
+        for j in range(i+1, num_of_employees):
             score = 0
-            a = avaConnectionStrengths[employeeIDs[i]][employeeIDs[j]]
-            b = avaConnectionStrengths[employeeIDs[j]][employeeIDs[i]]
+            a = ava_connection_strengths[employee_ids[i]][employee_ids[j]]
+            b = ava_connection_strengths[employee_ids[j]][employee_ids[i]]
         
             if a>=50 and b>=50:
                 score = (a+b)/2
                 if score > 80:
-                    G2.add_edge(employeeIDs[i],employeeIDs[j], weight = score)
+                    G2.add_edge(employee_ids[i],employee_ids[j],weight = score)
     
-    graphs_path = "/app/services/commands/connections_graphs/"
+    graphs_path = "./services/commands/connections_graphs/"
     if not os.path.exists(graphs_path):
         os.makedirs(graphs_path)
 
     layout = nx.planar_layout(G2)
-    for i in range(numOfEmployees):
-        color_map = numOfEmployees * ["royalblue"]
+    for i in range(num_of_employees):
+        color_map = num_of_employees * ["royalblue"]
         color_map[i] = "red"
         plt.show()
         nx.draw(G2, with_labels = False , node_color=color_map, width=2, edge_color = "royalblue", pos = layout)
