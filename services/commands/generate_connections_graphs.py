a93b5aa5f2ceca4eaa3884fc4733e249a2b7fbe7
Borna Lindic <borna.lindic@gmail.com>
2022-09-27 14:39:22 +0000

    Fixed by Juraj

--- services/commands/generate_connections_graphs.py
+++ services/commands/generate_connections_graphs.py
@@ -10,67 +10,67 @@ import forge
 
 @cli.command()
 def generate_connections_graphs():
-    forge.setup("rule_engine")
-    keeper = MongoClientKeeper()
-    culture_master = keeper.ruleEngineDB.summary.find_one(
-        {"agentId": "CULTURE_MASTER"})
-
-    ava_connection_strengths = culture_master["agents#CultureMaster"]["CULTURE_MASTER"]["employeeConnectionStrengths"]
-    G = nx.Graph()
-    edges = []
-
-    for i in ava_connection_strengths:
-        G.add_node(i)
-        for j in ava_connection_strengths[i]:
-            if i == j:
-                continue
-            weight = ava_connection_strengths[i][j]
-            if weight > 80:
-                edges.append((i, j))
-                G.add_edge(i, j, weight=weight)
-
     graphs_path = "./services/commands/connections_graphs/"
     if not os.path.exists(graphs_path):
         os.makedirs(graphs_path)
 
-    edges_map = defaultdict(list)
-    for edge in edges:
-        edges_map[edge[0]].append(edge[1])
+    forge.setup("rule_engine")
+    keeper = MongoClientKeeper()
+    culture_master = keeper.ruleEngineDB.summary.find_one({"agentId": "CULTURE_MASTER"})
+    ava_connection_strengths = culture_master["agents#CultureMaster"]["CULTURE_MASTER"]["employeeConnectionStrengths"]
     
-    for i in ava_connection_strengths:
-        new_graph = G.copy()
-        current_employee = i
-        layout = nx.spring_layout(G,k = 15)
+    for current_employee in ava_connection_strengths.keys():
+        G = nx.Graph()
 
         color_map = []
-        for node in new_graph:
-            if node == current_employee:
-                color_map.append('#c4c4c4')
+        edge_color_map = {}
+        edge_color_list = []
+        
+        for i in ava_connection_strengths.keys():
+            G.add_node(i)
+            if i == current_employee:
+                color_map.append('red')
             else:
-                color_map.append('aquamarine') 
+                color_map.append('aquamarine')
 
-        edge_color_map = []
-        edges_to_remove = []
-        for edge in new_graph.edges():
-            if current_employee in edge:
-                if edge[0] in edges_map.get(edge[1], []) and edge[1] in edges_map.get(edge[0], []):
-                    edge_color_map.append('red')
-                if edge[0] in edges_map.get(edge[1], []) or edge[1] in edges_map.get(edge[0], []):
-                    edge_color_map.append('yellow')
-                else:
-                    edge_color_map.append('#c4c4c4')
-            else:
-                if edge[0] in edges_map.get(edge[1], []) and edge[1] in edges_map.get(edge[0], []):
-                    edge_color_map.append('#c4c4c4')
-                else: 
-                    edges_to_remove.append(edge)
+        for other_employee in ava_connection_strengths[current_employee].keys():
+            if other_employee == current_employee:
+                continue
 
-        for edge in edges_to_remove:
-            new_graph.remove_edge(edge[0], edge[1])
+            if other_employee not in ava_connection_strengths.keys():
+                # Employee didn't answer
+                continue
 
+            employee_connection_strength = ava_connection_strengths[current_employee][other_employee]
+            other_connection_strength = ava_connection_strengths[other_employee][current_employee]
+            if employee_connection_strength > 80 and other_connection_strength > 80:
+                G.add_edge(current_employee, other_employee, weight=100)
+                edge_color_map[(current_employee, other_employee)] = "red"
+            elif employee_connection_strength > 80:
+                G.add_edge(current_employee, other_employee, weight=50)
+                edge_color_map[(current_employee, other_employee)] = "yellow"
+
+        for i in range(len(ava_connection_strengths)):
+            e1 = list(ava_connection_strengths.keys())[i]
+            if e1 == current_employee:
+                continue
+
+            for j in range(i+1, len(ava_connection_strengths)):
+                e2 = list(ava_connection_strengths.keys())[j]
+                if e2 == current_employee:
+                    continue
+
+                if ava_connection_strengths[e1][e2] > 80 and ava_connection_strengths[e2][e1] > 80:
+                    G.add_edge(e1, e2, weight=100)
+                    edge_color_map[(e1, e2)] = "#c4c4c4"
+
+        for edge in G.edges():
+            edge_color_list.append(edge_color_map.get(edge, edge_color_map.get(edge[::-1])))
+
+        layout = nx.spring_layout(G, k=15)
         plt.show()
-        nx.draw(new_graph, with_labels=False, node_color=color_map, width=0.6,
-                edge_color=edge_color_map, pos=layout, node_size=100)
-        plt.savefig(graphs_path + "{}.png".format(i), format="PNG")
+        nx.draw(G, with_labels=False, width=0.6, node_color=color_map,
+                edge_color=edge_color_list, pos=layout, node_size=100)
+        plt.savefig(f"{graphs_path}{current_employee}.png", format="PNG")
         plt.close()
 
