b8a63d11644870413ef956a8766a194478fc55ee
ivan.dukic <ivandukic97@gmail.com>
2022-10-06 13:28:01 +0000

    PostHog implemented

--- services/rule_engine/src/main/java/utils/Settings.java
+++ services/rule_engine/src/main/java/utils/Settings.java
@@ -1,12 +1,16 @@
 package utils;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import java.util.Map;
 
 public class Settings {
     private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
     public static String ARMORY_SITE_URL;
     public static String MODULE;
 
+    public static final Map<String, String> env = System.getenv();
+    public static String POSTHOG_API_KEY = env.get("POSTHOG_API_KEY");
+    
     public Settings() {
     }
 
