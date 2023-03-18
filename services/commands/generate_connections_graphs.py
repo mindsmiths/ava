eb1628f4555e5e41e7c56bd125c8112072c605ee
bazul <domagoj.blazanin@mindsmiths.com>
2023-03-18 22:00:41 +0100

    basic matching working

--- services/commands/generate_connections_graphs.py
+++ services/commands/generate_connections_graphs.py
@@ -1,11 +1,14 @@
 import os
+from pprint import pprint
 
 import matplotlib.pyplot as plt
 import networkx as nx
+from forge.utils import random_string
 
 from forge_cli.admin import cli
 from forge.utils.mongo import MongoClientKeeper
 import forge
+
 
 @cli.command()
 def generate_connections_graphs():
@@ -16,8 +19,9 @@ def generate_connections_graphs():
     forge.setup("rule_engine")
     keeper = MongoClientKeeper()
     culture_master = keeper.ruleEngineDB.summary.find_one({"agentId": "CULTURE_MASTER"})
-    employee_dictionary = culture_master["agents#CultureMaster"]["CULTURE_MASTER"]["lunchCompatibilities"]
-    ava_employees = culture_master["agents#CultureMaster"]["CULTURE_MASTER"]["employees"]
+    pprint(culture_master)
+    employee_dictionary = keeper.pairingAlgorithmDB.lunchCompatibilities.find_one({})
+    ava_employees = culture_master["facts"]["agents#CultureMaster"]["CULTURE_MASTER"]["employees"]
 
     matches = employee_dictionary["edges"]
 
@@ -32,7 +36,7 @@ def generate_connections_graphs():
             # Employee didn't answer
             continue
         else:
-            ava_name_keeper[val["id"]] = val["firstName"]
+            ava_name_keeper[val["id"]] = val["firstName"] + " " + val["lastName"] + " " + random_string(5)
 
     G = nx.Graph()
 
@@ -43,7 +47,7 @@ def generate_connections_graphs():
     for match in matches:
         G.add_edge(ava_name_keeper[match["first"]], ava_name_keeper[match["second"]], width=float(match["edgeWeight"]))
 
-    print('Saving...')
+    print(f'Saving {graphs_path}connection_graph.png ... ')
     nx.draw(G, node_size=450, with_labels=True)
 
     plt.axis('equal')
